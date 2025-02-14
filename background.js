5205063587fc64574b3e08d9e06deb120c70ffe1
Akram-Adam <akramadam050@gmail.com>
2025-02-14 15:45:15 +0000

    done

--- background.js
+++
@@ -1,9 +0,0 @@
-// background.js
-
-// Initialize default values on extension install
-chrome.runtime.onInstalled.addListener(() => {
-    chrome.storage.sync.set({
-        widgetType: 'quote', // default is quote
-        customWidgetText: 'This space is now a custom widget!'
-    });
-  });
