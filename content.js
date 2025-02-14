34d8659e3e30cfcc8685de93cc4625374e388602
esraa <esraa2023112@gamil.com>
2025-02-14 20:43:33 +0200

    update ads const

--- content.js
+++ content.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const ads = document.querySelectorAll("iframe, .ad, .ads, [id*='ad']");
+  const ads = document.querySelectorAll(".ad-container, #ad-banner, iframe, .ad, .ads, [id*='ad']");
   ads.forEach(ad => {
     const message = document.createElement("div");
     message.className = "motivational-message";
